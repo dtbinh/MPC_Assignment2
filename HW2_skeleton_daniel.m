3a225dc60d4efe58f700576767fc4dace7e1a4a0
Danpihl <daniel.pihlquist@gmail.com>
2015-02-25 19:42:39 +0100

    Färdig kord

--- HW2_skeleton_daniel.m
+++ HW2_skeleton_daniel.m
@@ -9,7 +9,7 @@
 %******************* Initialization block *********************************
 
 clear;
-close all
+% close all
 clc
 tf=50;                  % number of simulation steps
 
@@ -36,7 +36,7 @@ n = size(A,1); % n is the dimension of the state
 m = size(B,2); % m is the dimension of the control signal
 p = size(C,1); % p is the dimension of the measured output
 
-d = 0.01*[zeros(1*tf/5,1); ones(4*tf/5,1)]; %unmeasured disturbance trajectory
+d = 0.01*[zeros(1*tf/5,1); ones(4*tf/5,1)]'; %unmeasured disturbance trajectory
 
 x0 = [0.01;1;0.1]; % initial condition of system's state
 
@@ -67,12 +67,24 @@ end
 Ae = [A, Bd; zeros(nd,n), eye(nd)];
 Be = [B;zeros(nd + n - size(B, 1), m)];
 Ce = [C Cd];
+O = obsv(Ae, Ce);
 
+Or = rank(O);
+
+if example == 'c'
+    Bdist = [zeros(n,nd); eye(nd)];
+    SYS = ss(Ae,[Be Bdist],Ce,[],60);
+else
+    SYS = ss(Ae, Be, Ce, zeros(size(Ce, 1), size(Be, 2)), 60);
+end
 % Calculate observer gain 
-SYS = ss(Ae, Be, Ce, zeros(size(Ce, 1), size(Be, 2)), 60);
-[KEST, Le, P] = kalman(SYS, eye(2)*0.01, eye(3));
+
+Qn = eye(2)*10000;
+Rn = eye(3);
+[KEST, Le, P] = kalman(SYS, Qn, Rn, 'delayed');
 
 
+% eig(Ae - Le*Ce);
 
 %==========================================================================
 % Prepare for computing steady state targets
@@ -85,12 +97,14 @@ Cz = H*C;
 
 As = [eye(3)-A, -B;
       Cz, zeros(2,2)];
+Adet = [eye(3)-A, -Bd;
+      C, Cd];
+    
+n+nd
+rank(Adet)
 
-dhat = 0.2*ones(nd,1);
 zsp = [0; 0];
- 
-bs = [Bd*dhat; -H*Cd*dhat];
-xs = As\bs;
+
 
 
 %==========================================================================
@@ -108,18 +122,14 @@ R = 0.01*eye(m);        % control penalty
 % Build Hessian Matrix
 %=================================
 
-%     YOUR MODIFIED CODE FROM ASSIGNMENT 1 GOES HERE
-
 Hm = [[kron(eye(N-1), Q), zeros((N-1)*n, n); zeros(n, (N-1)*n), Pf], zeros(N*n, M*m); zeros(M*m, N*n), kron(eye(M), R)];
-
-Aeq = [[kron(eye(N), eye(n)) + kron([zeros(1,N); [eye(N-1), zeros(N-1, 1)]], -A)], [kron(eye(M), B); [zeros((N-M)*n, m*(M-1)), kron(ones(N-M, 1), -B)]]];
-AA = [A; zeros((N-1)*n,n)];
 
 %==========================================
 % Equality Constraints
 %==========================================
 
-%     YOUR MODIFIED CODE FROM ASSIGNMENT 1 GOES HERE
+Aeq = [[kron(eye(N), eye(n)) + kron([zeros(1,N); [eye(N-1), zeros(N-1, 1)]], -A)], [kron(eye(M), -B); [zeros((N-M)*n, m*(M-1)), kron(ones(N-M, 1), -B)]]];
+AA = [A; zeros((N-1)*n,n)];
 
 %==========================================
 % Inequality Constraints
@@ -143,24 +153,21 @@ options = optimset('Algorithm', solver, 'Display', 'off');
 %==========================================================================
     
 % Initialization
-    
-% YOUR CODE GOES HERE - INITIALIZE ALL VARIABLES NEEDED 
 
-% xhat = [x0; zeros(nd,1)];
-xhat = [zeros(nd+n,1)];
+xhat = [x0; zeros(nd,1)];
+dhat = zeros(nd,1);
 xk = x0;
 yk = C*xk;
 uk = zeros(m,1);
 
-xvec = [];
-uvec = [];
-xhatvec = [];
-dhatvec = [];
-yvec = [];
-xusvec = [];
 
+xvec = x0;
+xhatvec = xhat;
+uvec = uk;
+dhatvec = dhat;
+yvec = C*xhat(1:n);
+realy = [];
 % Simulate closed-loop system 
-    
 
 
 for k = 1:tf
@@ -169,7 +176,6 @@ for k = 1:tf
     % Update the observer state xhat(k|k-1)
     %======================================
 
-    %         YOUR CODE GOES HERE
     xhat = Ae*xhat + Be*uk + Le*(yk - Ce*xhat);
     dhat = xhat(n+1:end);
 
@@ -177,38 +183,30 @@ for k = 1:tf
     % Update the process state x(k) and output y(k)
     %==============================================
 
-    xk = A*xk + B*uk + Bd*ones(nd,1)*d(k);
+    xk = A*xk + B*uk + Bp*d(k);
     yk = C*xk;
 
     %=========================================
     % Calculate steady state targets xs and us
     %=========================================
 
-
     bs = [Bd*dhat; -H*Cd*dhat];
     xus = As\bs;
     xs = xus(1:n);
     us = xus((n+1):end);
 
-%         YOUR CODE GOES HERE
-
     %============================================
     % Solve the QP (for the deviation variables!)
     %============================================
 
-%     UPDATE RHS OF EQUALITY CONSTRAINT HERE
     dx = xhat(1:n) - xs;
     beq = AA*dx;
-%     beq = AA*xhat(1:n);
-        
+
     z = quadprog(Hm,[],Ain,Bin,Aeq,beq,[],[],[],options);
     
     du = z(N*n+1:N*n+m);
-    
     uk = du + us;
-        
-%     CALCULATE THE NEW CONTROL SIGNAL HERE
-    % NOTE THAT YOU NEED TO GO FROM DEVIATION VARIABLES TO 'REAL' ONES!
+    
 
     %===============================        
     % Store current variables in log 
@@ -217,28 +215,47 @@ for k = 1:tf
     xhatvec = [xhatvec, xhat];
     uvec = [uvec, uk];
     dhatvec = [dhatvec, dhat];
-    yvec = [yvec, yk];
-    xusvec = [xusvec, xus];
+    yvec = [yvec, C*xhat(1:n)];
+    realy = [realy, C*xk];
 %     YOUR CODE GOES HERE
 
 end % simulation loop
 
-figure(1)
-hold on
-% plot(xusvec(5, :))
-% plot(xvec(3,:))
-% plot(xhatvec(3,:), 'r--');
-% plot(dhatvec(1,:))
-plot(yvec(3,:))
-% plot(d)
-
-% axis([0, 30, -1, 10])
-
-
-% figure(2)
 
 %==========================================================================
 % Plot results
 %==========================================================================
-        
-% YOUR CODE GOES HERE
+figure(1)
+clf
+subplot(3, 1, 1)
+hold on
+plot(yvec(1,:), 'b')
+plot(realy(1,:), 'b--')
+xlabel('Minutes')
+
+legend('Concentration observed', 'Concentration real')
+subplot(3, 1, 2)
+hold on
+plot(yvec(2,:), 'r')
+plot(realy(2,:), 'r--')
+xlabel('Minutes')
+
+legend('Temperature observed', 'Temperature real')
+subplot(3, 1, 3)
+hold on
+plot(yvec(3,:), 'k')
+plot(realy(3,:), 'k--')
+xlabel('Minutes')
+legend('Tank level observed', 'Tank level real')
+
+
+figure(2)
+clf
+subplot(2,1,1)
+plot(uvec(1, :), 'g')
+xlabel('Minutes')
+legend('Control signal 1')
+subplot(2,1,2)
+plot(uvec(2, :), 'r')
+xlabel('Minutes')
+legend('Control signal 2')
--- matrices.m
+++ matrices.m
@@ -1,31 +1,22 @@
 clc, clear
 
-syms q r p x1 x2 a1 a12 a2 b1 b12 b2
-A = [a1, a12; a12, a2];
-B = [b1, b12; b12, b2];
+syms q r p x1 x2 a11 a12 a13 a21 a22 a23 a31 a32 a33 b11 b12 b21 b22 b31 b32
+A = [a11, a12, a13; a21, a22, a23; a31, a32, a33];
+B = [b11, b12; b21, b22; b31, b32];
 xk = [x1; x2];
 Q = [q, 0; 0, q];
 R = [r, 0; 0, r];
 Pf = [p, 0; 0, p];
 
-n = 2;
+n = 3;
 m = 2;
 
 N = 10;
 M = 3;
 
-% AA = [A; zeros((N-1)*n,n)]
-% beq = AA*xk
-H = [[kron(eye(N-1), Q), zeros((N-1)*n, n); zeros(n, (N-1)*n), Pf], zeros(N*n, M*m); zeros(M*m, N*n), kron(eye(M), R)];
+Aeq = [[kron(eye(N), eye(n)) + kron([zeros(1,N); [eye(N-1), zeros(N-1, 1)]], -A)], [kron(eye(M), -B); [zeros((N-M)*n, m*(M-1)), kron(ones(N-M, 1), -B)]]];
 
-% Aeq = [[kron(eye(N), eye(n)) + kron([zeros(1,N); [eye(N-1), zeros(N-1, 1)]], -A)], [kron(eye(M), B); [zeros((M)*n, m*(M-1)), kron(ones(M, 1), -B)]]];
-
-a = [kron(eye(N), eye(n)) + kron([zeros(1,N); [eye(N-1), zeros(N-1, 1)]], -A)];
-b = [kron(eye(M), B); [zeros((N-M)*n, m*(M-1)), kron(ones(N-M, 1), -B)]];
-
-Aeq = [[kron(eye(N), eye(n)) + kron([zeros(1,N); [eye(N-1), zeros(N-1, 1)]], -A)], [kron(eye(M), B); [zeros((N-M)*n, m*(M-1)), kron(ones(N-M, 1), -B)]]];
-
-[a,b]
+Aeq
 % size(a)
 % size(b)
 % kron(eye(M), B)
